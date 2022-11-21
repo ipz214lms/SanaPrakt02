fdbd720d9fc3dcb27d2e91c161a722b2d2b30f92
ipz214_lms <ipz214_lms@student.ztu.edu.ua>
2022-11-21 18:47:57 +0200

    18. change 2 lines

--- SanaPrakt02/Task1/Program.cs
+++ SanaPrakt02/Task1/Program.cs
@@ -1,6 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
-Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
-Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
+Console.WriteLine("Text1");
+Console.WriteLine("Text2");
+Console.WriteLine("Text3");
 Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
 Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
